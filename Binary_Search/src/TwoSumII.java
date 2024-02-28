98c934c443d7301cd0ceb12f1b92cecc5d665ab7
Akshat Jain <jain.akshat.0730@gmail.com>
2024-02-29 02:20:38 +0530

    Create TwoSumII.java

---
+++ Binary_Search/src/TwoSumII.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class TwoSumII {
+    public static void main(String[] args) {
+        
+    }
+    static int[] twoSum(int[] numbers, int target) {
+        int start = 0;
+        int end = numbers.length - 1;
+        while(numbers[start] + numbers[end] != target){
+            if(numbers[start] + numbers[end] < target){
+                start++;
+            }
+            else end--;
+        }
+        return new int[] {start + 1, end + 1};
+    }
+}
