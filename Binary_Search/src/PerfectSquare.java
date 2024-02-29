455b8a4cd4a72509f7ededdcbd9c2639330021e9
Akshat Jain <jain.akshat.0730@gmail.com>
2024-03-01 01:09:46 +0530

    Create PerfectSquare.java

---
+++ Binary_Search/src/PerfectSquare.java
@@ -0,0 +1,23 @@
+class PerfectSquare {
+    public static void main(String[] args) {
+        System.out.println(isPerfectSquare(16));
+    }
+    static boolean isPerfectSquare(int num) {
+        int start = 0;
+        int end = num;
+
+        while(start <= end){
+            long mid = start + (end-start)/2;
+            long squareMid = mid*mid;
+
+            if(squareMid == num){
+                return true;
+            }else if(squareMid > num){
+                end = (int) mid-1;
+            }else {
+                start = (int) mid+1;
+            }
+        }
+        return false;
+    }
+}
