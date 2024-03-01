48486cb8d53017b82091976569af56e669314b1b
Akshat Jain <jain.akshat.0730@gmail.com>
2024-03-02 00:27:54 +0530

    code for question number 35 leetcode

---
+++ Binary_Search/src/SearchInsertPosition.java
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/search-insert-position/description/
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+
+    }
+    public int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length;
+        while (end - start > 1) {
+            int i = start + (end - start) / 2;
+            if (target > nums[i]) {
+                start = i;
+            } else {
+                end = i;
+            }
+        }
+        if (target <= nums[start]) {
+            return start;
+        } else {
+            return end;
+        }
+    }
+}
