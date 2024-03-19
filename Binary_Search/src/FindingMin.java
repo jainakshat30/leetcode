c365ba5944f41d97eb9d9b932a6c36c9c56a84a3
Akshat Jain <jain.akshat.0730@gmail.com>
2024-03-19 10:50:55 +0530

    yaman ko dikha rha hu

--- Binary_Search/src/FindingMin.java
+++ Binary_Search/src/FindingMin.java
@@ -7,6 +7,7 @@ class FindingMin {
     }
     public static int findMin(int[] nums) {
         int start = 0;
+        int akshat = 0;
         int end = nums.length - 1;
         while(start <= end){
             int mid = start + (end-start)/2;
