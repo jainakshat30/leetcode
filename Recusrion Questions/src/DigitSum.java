d7d6c92eea6fb1f3135f88ca1272aece50d25157
Akshat Jain <jain.akshat.0730@gmail.com>
2024-03-22 18:38:43 +0530

    better method

--- Recusrion Questions/src/DigitSum.java
+++ Recusrion Questions/src/DigitSum.java
@@ -1,8 +1,10 @@
 public class DigitSum {
     public static void main(String[] args) {
-        int ans = sumDigit(1342);
+        int ans = sum(1342);
         System.out.println(ans);
     }
+
+    //my method Space complexity == ?
 
     static int sumDigit(int n){
         if(n == 0){
@@ -11,5 +13,14 @@ public class DigitSum {
         int sum = 0;
         sum = n % 10;
         return sum + sumDigit(n/10);
+    }
+
+    //kunal's method Space complexity == O(log(n))
+
+    static int sum(int n){
+        if (n == 0){
+            return 0;
+        }
+        return n%10 + sum(n/10);
     }
 }
