132e677445517e6c65df234ea9f5852b8cec5311
Akshat Jain <jain.akshat.0730@gmail.com>
2024-03-27 14:52:12 +0530

    linear search using reecusrion

---
+++ Recusrion Questions/Array/src/LinearSearch.java
@@ -0,0 +1,42 @@
+public class LinearSearch {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,8,9};
+        System.out.println(findIndex(arr,9,0));
+    }
+
+    //my method
+    static boolean search(int[] arr, int target, int index){
+        if(index > arr.length-1){  //base condition if index is more than the length of the arr.length just return false
+            return false;
+        }
+        if(arr[index] == target){
+            return true;
+        }
+        return arr[index] == target || search(arr,target,index+1);
+    }
+
+    //kunal's method
+    static boolean search1(int[] arr, int target, int index){
+
+        if(index == arr.length){
+            return false;
+        }
+        return arr[index] == target || search(arr,target,index+1);
+    }
+
+
+//to find index value
+    static int findIndex(int[] arr, int target, int index){
+
+        if(index == arr.length){
+            return -1;
+        }
+        if (arr[index] == target ){
+            return index;
+        }
+        else{
+            return findIndex(arr,target,index+1);
+        }
+
+    }
+}
