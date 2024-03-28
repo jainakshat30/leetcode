301f05fcbe9f0f50c60c906a3aed7a9a976fa49b
Akshat Jain <jain.akshat.0730@gmail.com>
2024-03-28 12:34:16 +0530

    find all index using recursion

--- Recusrion Questions/Array/src/Find.java
+++ Recusrion Questions/Array/src/Find.java
@@ -1,20 +1,49 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Find {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4,4,8,9};
-        findAllIndex(arr,4,0);
-        System.out.println(list);
+//       ArrayList<Integer> ans = findAllIndex1(arr,4,0,new ArrayList<>());
+//        System.out.println(ans);
+        System.out.println(findAllIndex2(arr,4,0));
     }
-    static ArrayList<Integer> list = new ArrayList<>();
-     static void findAllIndex(int[] arr, int target, int index){
+
+
+
+    //     static void findAllIndex(int[] arr, int target, int index){
+//        if(index == arr.length){
+//            return;
+//        }
+//        if(arr[index] == target){
+//            list.add(index);
+//        }
+//        findAllIndex(arr, target,index + 1);
+//
+//    }
+
+    static ArrayList<Integer> findAllIndex1(int[] arr, int target, int index,ArrayList<Integer> list){
         if(index == arr.length){
-            return;
+            return list;
         }
         if(arr[index] == target){
             list.add(index);
         }
-        findAllIndex(arr, target,index + 1);
+        return findAllIndex1(arr, target,index + 1,list);
+    }
+    static ArrayList<Integer> findAllIndex2(int[] arr, int target, int index){
+        ArrayList<Integer> list = new ArrayList<>();
+        if (index == arr.length){
+            return list;
+        }
+        //this will contain answer for that function call only
+        if(arr[index] == target){
+            list.add(index);
+        }
+        ArrayList<Integer> ansFromBelowCalls = findAllIndex2(arr, target,index + 1);
 
+        list.addAll(ansFromBelowCalls);
+
+        return list;
     }
 }
