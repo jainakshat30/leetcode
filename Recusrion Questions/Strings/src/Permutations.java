1bbee5ca8c5b0486903389eeb8f807385ecb6d1f
Akshat Jain <jain.akshat.0730@gmail.com>
2024-04-17 13:50:13 +0530

    Update Permutations.java

--- Recusrion Questions/Strings/src/Permutations.java
+++ Recusrion Questions/Strings/src/Permutations.java
@@ -3,9 +3,10 @@ import java.util.ArrayList;
 
 public class Permutations {
     public static void main(String[] args) {
-        ArrayList<String> ans = permutationsList("","abc");
-
-        System.out.print(ans);
+//        ArrayList<String> ans = permutationsList("","abc");
+//
+//        System.out.print(ans);
+        System.out.println(permutationCount("","abc"));
     }
 
 
@@ -38,6 +39,20 @@ public class Permutations {
 
         }
         return ans;
+    }
+
+    static int permutationCount(String p, String up){
+        if (up.isEmpty()) {
+            return 1;
+        }
+        int count = 0;
+        char ch = up.charAt(0);
+        for (int i = 0; i <= p.length(); i++) {
+            String f = p.substring(0, i);
+            String s = p.substring(i,p.length());
+            count = count + permutationCount(f + ch + s, up.substring(1));
+        }
+        return count;
     }
 }
 
