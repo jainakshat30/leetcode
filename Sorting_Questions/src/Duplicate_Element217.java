dcc9807e01665bb1b239d9837116dba2bf4465cd
Akshat Jain <jain.akshat.0730@gmail.com>
2024-04-21 18:35:43 +0530

    Update Duplicate_Element217.java

--- Sorting_Questions/src/Duplicate_Element217.java
+++ Sorting_Questions/src/Duplicate_Element217.java
@@ -9,6 +9,10 @@ public class Duplicate_Element217 {
     }
 
     static boolean duplicate(int[] arr){
+       //Approach 1 -- Brute Force (use two for loops to check if the number duplicates or not) time complexity -- O(n*n) ****TLE***
+
+
+       //Approach 2 -- Sorting the array and checking the adjacent numbers
         Arrays.sort(arr);
         for (int i = 0; i < arr.length - 1; i++) {
             if (arr[i] == arr[i+1]) {
