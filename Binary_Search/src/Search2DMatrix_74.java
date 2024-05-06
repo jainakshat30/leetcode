b82f1172faa2c823bf1f708d36ac360fc84f1b8e
Akshat Jain <jain.akshat.0730@gmail.com>
2024-05-06 23:53:58 +0530

    Update Search2DMatrix_74.java

--- Binary_Search/src/Search2DMatrix_74.java
+++ Binary_Search/src/Search2DMatrix_74.java
@@ -1,5 +1,20 @@
 class Search2DMatrix_74 {
-    public boolean searchMatrix(int[][] matrix, int target) {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 3, 5, 7},
+                {10, 11, 16, 20},
+                {23, 30, 34, 60}
+        };
+
+        int target1 = 3;   // Target present in the matrix
+        int target2 = 13;  // Target not present in the matrix
+
+        // Test the searchMatrix function
+        System.out.println(searchMatrix(matrix, target1));  // Output: true
+        System.out.println(searchMatrix(matrix, target2));  // Output: false
+
+    }
+    static boolean searchMatrix(int[][] matrix, int target) {
         if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
             return false;
         }
