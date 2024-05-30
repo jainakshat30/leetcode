731bae2f2d555799880b728deb4c6fb69308261c
Akshat Jain <jain.akshat.0730@gmail.com>
2024-05-30 19:25:33 +0530

    minor change

---
+++ Sorting_Questions/src/squareSorted.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class squareSorted {
+    public int[] sortedSquares(int[] nums) {
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length; i++){
+            nums[i] *= nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
