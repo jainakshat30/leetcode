f1e191766ae5b3d7b3dca228b3da0b1467669964
Akshat Jain <jain.akshat.0730@gmail.com>
2024-06-05 23:47:08 +0530

    solution to question number 1608

---
+++ Sorting_Questions/src/specialArray.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class specialArray {
+    public int specialArray(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        
+        if (nums[0] >= n) return n;
+        
+        for (int i = 1; i <= n; i++) {
+            if (nums[n - i] >= i && (n - i - 1 < 0 || nums[n - i - 1] < i)) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
