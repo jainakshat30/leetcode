41c2d1b7e1304dd9b38f3d3cd080cd03564d8e40
Akshat Jain <123355976+jainakshat30@users.noreply.github.com>
2026-08-02 01:16:58 +0530

    Time: 1 ms (52.06%), Space: 42.6 MB (89.68%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        int[][] dp = new int[n][n];
+        
+        // Base case: subarrays of length 1
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = nums[i];
+        }
+        
+        // length = current subarray size
+        for (int length = 2; length <= n; length++) {
+            for (int i = 0; i + length - 1 < n; i++) {
+                int j = i + length - 1;
+                int pickLeft  = nums[i] - dp[i + 1][j];
+                int pickRight = nums[j] - dp[i][j - 1];
+                dp[i][j] = Math.max(pickLeft, pickRight);
+            }
+        }
+        
+        // If Player 1's best difference on [0..n-1] is >= 0, they can win or tie
+        return dp[0][n - 1] >= 0;
+
+
+        
+    }
+}
