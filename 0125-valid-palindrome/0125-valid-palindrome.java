3d5595416a0659d122fe57659836f11247db6e13
Akshat Jain <123355976+jainakshat30@users.noreply.github.com>
2026-08-09 14:31:49 +0530

    Time: 2 ms (99.32%), Space: 44.4 MB (64.5%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,17 +1,41 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder cleaned = new StringBuilder();
+        // StringBuilder cleaned = new StringBuilder();
 
-        for(char c : s.toCharArray()){
-            if(Character.isLetterOrDigit(c)){
-                cleaned.append(Character.toLowerCase(c));
+        // for(char c : s.toCharArray()){
+        //     if(Character.isLetterOrDigit(c)){
+        //         cleaned.append(Character.toLowerCase(c));
+        //     }
+        // }
+
+        // String result = cleaned.toString();
+        // String reversed = cleaned.reverse().toString();
+
+        // return result.equals(reversed);
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right){
+            while(left < right && !Character.isLetterOrDigit(s.charAt(left))){
+                left++;
             }
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            char leftChar = Character.toLowerCase(s.charAt(left));
+            char rightChar = Character.toLowerCase(s.charAt(right));
+            if (leftChar != rightChar) {
+                return false;
+            }
+            left++;
+            right--;
         }
 
-        String result = cleaned.toString();
-        String reversed = cleaned.reverse().toString();
-
-        return result.equals(reversed);
+        return true;
 
     }
+
 }
