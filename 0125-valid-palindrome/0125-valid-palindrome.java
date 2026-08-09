a961a8847216eba413b4201cffdf151e6dee97c4
Akshat Jain <123355976+jainakshat30@users.noreply.github.com>
2026-08-09 14:28:49 +0530

    Time: 4 ms (45.81%), Space: 45.1 MB (37.43%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder cleaned = new StringBuilder();
+
+        for(char c : s.toCharArray()){
+            if(Character.isLetterOrDigit(c)){
+                cleaned.append(Character.toLowerCase(c));
+            }
+        }
+
+        String result = cleaned.toString();
+        String reversed = cleaned.reverse().toString();
+
+        return result.equals(reversed);
+
+    }
+}
