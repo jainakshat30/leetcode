32e5e81c17a48b81690836c73d350b223c8d33ba
Akshat Jain <123355976+jainakshat30@users.noreply.github.com>
2026-08-20 00:12:46 +0530

    Time: 3 ms (14.5%), Space: 44.9 MB (33.03%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -2,33 +2,33 @@ import java.util.*;
 
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String>[][] memo = new ArrayList[n + 1][n + 1];
-        return helper(n, n, memo);
+        List<String>[][] dp = new ArrayList[n + 1][n + 1];
+        return helper(n, n, dp);
     }
 
-    private List<String> helper(int open, int close, List<String>[][] memo) {
-        if (memo[open][close] != null) return memo[open][close];
+    private List<String> helper(int open, int close, List<String>[][] dp) {
+        if (dp[open][close] != null) return dp[open][close];
 
         List<String> ans = new ArrayList<>();
 
         if (open == 0 && close == 0) {
             ans.add("");
-            return memo[open][close] = ans;
+            return dp[open][close] = ans;
         }
 
         if (open > 0) {
-            for (String s : helper(open - 1, close, memo)) {
+            for (String s : helper(open - 1, close, dp)) {
                 ans.add("(" + s);
             }
         }
 
         if (close > open) {
-            for (String s : helper(open, close - 1, memo)) {
+            for (String s : helper(open, close - 1, dp)) {
                 ans.add(")" + s);
             }
         }
 
-        memo[open][close] = ans;
+        dp[open][close] = ans;
         return ans;
     }
 }
